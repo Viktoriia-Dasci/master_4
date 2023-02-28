0165d6fbf5d0130798fbae6342b4f2de432ba8cf
Viktoriia-Dasci <74453524+Viktoriia-Dasci@users.noreply.github.com>
2023-02-28 22:35:38 +0100

    Update models_keras.py

--- models_keras.py
+++ models_keras.py
@@ -216,13 +216,13 @@ def plot_acc_loss(model_history):
 
 # plot_acc_loss(history_densenet121)
 
-history_vit = model_train(model_name = tfa.image.ViTModel(include_top=False, weights='imagenet', input_shape=(224,224,3), classes=2))
+# history_vit = model_train(model_name = tfa.image.ViTModel(include_top=False, weights='imagenet', input_shape=(224,224,3), classes=2))
 
-plot_acc_loss(history_vit)
+# plot_acc_loss(history_vit)
 
-history_deit = model_train(model_name = tfa.image.DeiT(include_top=False, pretrained=True, input_shape=(224,224,3), classes=2))
+# history_deit = model_train(model_name = tfa.image.DeiT(include_top=False, pretrained=True, input_shape=(224,224,3), classes=2))
 
-plot_acc_loss(history_deit)
+# plot_acc_loss(history_deit)
 
 history_Regnety = model_train(model_name = tfa.image.Regnety(include_top=False, pretrained=True, input_shape=(224,224,3), classes=2))
 
