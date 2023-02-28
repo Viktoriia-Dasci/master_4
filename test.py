91ff04f889db87a92fb2489a09776925b4d4a7ee
Viktoriia-Dasci <74453524+Viktoriia-Dasci@users.noreply.github.com>
2023-02-28 16:45:24 +0100

    Update test.py

--- test.py
+++ test.py
@@ -228,9 +228,8 @@ for inputs, labels, masks in test_dataloader:
   
     outputs, targets_, xe_loss_, gcam_losses_ = model(inputs, labels, masks, batch_size = inputs.size(0), dropout=nn.Dropout(0.79))
 
-    loss = xe_loss_.mean() + 0.9 * gcam_losses_.mean()
-    #loss = xe_loss_
-    
+    loss = xe_loss_.mean() + 0.575 * gcam_losses_.mean()
+   
     _, preds = torch.max(outputs, 1)  
 
     running_loss += loss.item() * inputs.size(0)
