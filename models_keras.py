9bfed528972952b7bff44a03787a1267212e65d6
Viktoriia-Dasci <74453524+Viktoriia-Dasci@users.noreply.github.com>
2023-03-05 20:14:05 +0100

    Update models_keras.py

--- models_keras.py
+++ models_keras.py
@@ -224,9 +224,9 @@ def plot_acc_loss(model_history):
 
 # plot_acc_loss(history_deit)
 
-history_Regnety = model_train(model_name = RegNetY(weights='imagenet', input_shape=(224, 224, 3), include_top=False))
+# history_Regnety = model_train(model_name = RegNetY(weights='imagenet', input_shape=(224, 224, 3), include_top=False))
 
-plot_acc_loss(history_Regnety)
+# plot_acc_loss(history_Regnety)
 
 # history_NFNet = model_train(model_name = tfa.image.NFNet(include_top=False, pretrained=True, input_shape=(224,224,3), classes=2))
 
@@ -238,87 +238,87 @@ plot_acc_loss(history_Regnety)
 # colors_red = ["#331313", "#582626", '#9E1717', '#D35151', '#E9B4B4']
 # colors_green = ['#01411C','#4B6F44','#4F7942','#74C365','#D0F0C0']
 
-# my_model_eff = load_model('/home/viktoriia.trokhova/master_4/<keras.engine.functional.Functional object at 0x7f0904dda9b0>.h5')
-# #print('efficientnet')
-# pred_eff = my_model_eff.predict(X_test)
-# pred_ready_eff = np.argmax(pred_eff,axis=1)
-# y_test_new_eff = np.argmax(y_test,axis=1)
+my_model_eff = load_model('/home/viktoriia.trokhova/master_4/<keras.engine.functional.Functional object at 0x7f0904dda9b0>.h5')
+print('efficientnet')
+pred_eff = my_model_eff.predict(X_test)
+pred_ready_eff = np.argmax(pred_eff,axis=1)
+y_test_new_eff = np.argmax(y_test,axis=1)
 
-# # print(classification_report(y_test_new_eff,pred_ready_eff))
+# print(classification_report(y_test_new_eff,pred_ready_eff))
 
-# # fig,ax=plt.subplots(1,1,figsize=(14,7))
-# # sns.heatmap(confusion_matrix(y_test_new_eff,pred_ready_eff),ax=ax,xticklabels=labels,yticklabels=labels,annot=True, fmt='g',
-# #            cmap=colors_green[::-1],alpha=0.7,linewidths=2,linecolor=colors_dark[3])
-# # fig.text(s='Heatmap of the Confusion Matrix',size=18,fontweight='bold',
-# #              fontname='monospace',color=colors_dark[1],y=0.92,x=0.28,alpha=0.8)
+# fig,ax=plt.subplots(1,1,figsize=(14,7))
+# sns.heatmap(confusion_matrix(y_test_new_eff,pred_ready_eff),ax=ax,xticklabels=labels,yticklabels=labels,annot=True, fmt='g',
+#            cmap=colors_green[::-1],alpha=0.7,linewidths=2,linecolor=colors_dark[3])
+# fig.text(s='Heatmap of the Confusion Matrix',size=18,fontweight='bold',
+#              fontname='monospace',color=colors_dark[1],y=0.92,x=0.28,alpha=0.8)
 
-# # plt.show()
+# plt.show()
 
-# test_loss, test_acc = my_model_eff.evaluate(X_test, y_test, verbose=2)
-# #acc = 0.81
+test_loss, test_acc = my_model_eff.evaluate(X_test, y_test, verbose=2)
+#acc = 0.81
 
-# print(f' Test accuracy: {test_acc:.3f} \n Test loss {test_loss:.3f}')
+print(f' Test accuracy: {test_acc:.3f} \n Test loss {test_loss:.3f}')
 
-# my_model_res = load_model('/home/viktoriia.trokhova/master_4/<keras.engine.functional.Functional object at 0x7f07cc32a950>.h5')
-# print('resnet')
-# pred_res = my_model_res.predict(X_test)
-# pred_ready_res = np.argmax(pred_res,axis=1)
-# y_test_new_res = np.argmax(y_test,axis=1)
+my_model_res = load_model('/home/viktoriia.trokhova/master_4/<keras.engine.functional.Functional object at 0x7f07cc32a950>.h5')
+print('resnet')
+pred_res = my_model_res.predict(X_test)
+pred_ready_res = np.argmax(pred_res,axis=1)
+y_test_new_res = np.argmax(y_test,axis=1)
 
-# # print(classification_report(y_test_new_res,pred_ready_res))
+# print(classification_report(y_test_new_res,pred_ready_res))
 
-# # fig,ax=plt.subplots(1,1,figsize=(14,7))
-# # sns.heatmap(confusion_matrix(y_test_new_res,pred_ready_res),ax=ax,xticklabels=labels,yticklabels=labels,annot=True, fmt='g',
-# #            cmap=colors_green[::-1],alpha=0.7,linewidths=2,linecolor=colors_dark[3])
-# # fig.text(s='Heatmap of the Confusion Matrix',size=18,fontweight='bold',
-# #              fontname='monospace',color=colors_dark[1],y=0.92,x=0.28,alpha=0.8)
+# fig,ax=plt.subplots(1,1,figsize=(14,7))
+# sns.heatmap(confusion_matrix(y_test_new_res,pred_ready_res),ax=ax,xticklabels=labels,yticklabels=labels,annot=True, fmt='g',
+#            cmap=colors_green[::-1],alpha=0.7,linewidths=2,linecolor=colors_dark[3])
+# fig.text(s='Heatmap of the Confusion Matrix',size=18,fontweight='bold',
+#              fontname='monospace',color=colors_dark[1],y=0.92,x=0.28,alpha=0.8)
 
-# # plt.show()
+# plt.show()
 
-# test_loss, test_acc = my_model_res.evaluate(X_test, y_test, verbose=2)
+test_loss, test_acc = my_model_res.evaluate(X_test, y_test, verbose=2)
 
-# print(f' Test accuracy: {test_acc:.3f} \n Test loss {test_loss:.3f}')
+print(f' Test accuracy: {test_acc:.3f} \n Test loss {test_loss:.3f}')
 
-# my_model_inception = load_model('/home/viktoriia.trokhova/master_4/<keras.engine.functional.Functional object at 0x7f07681711b0>.h5')
-# print('inception')
-# pred_incep = my_model_inception.predict(X_test)
-# pred_ready_incep = np.argmax(pred_incep,axis=1)
-# y_test_new_incep = np.argmax(y_test,axis=1)
+my_model_inception = load_model('/home/viktoriia.trokhova/master_4/<keras.engine.functional.Functional object at 0x7f07681711b0>.h5')
+print('inception')
+pred_incep = my_model_inception.predict(X_test)
+pred_ready_incep = np.argmax(pred_incep,axis=1)
+y_test_new_incep = np.argmax(y_test,axis=1)
 
-# # print(classification_report(y_test_new_incep,pred_ready_incep))
+# print(classification_report(y_test_new_incep,pred_ready_incep))
 
-# # fig,ax=plt.subplots(1,1,figsize=(14,7))
-# # sns.heatmap(confusion_matrix(y_test_new_incep,pred_ready_incep),ax=ax,xticklabels=labels,yticklabels=labels,annot=True, fmt='g',
-# #            cmap=colors_green[::-1],alpha=0.7,linewidths=2,linecolor=colors_dark[3])
-# # fig.text(s='Heatmap of the Confusion Matrix',size=18,fontweight='bold',
-# #              fontname='monospace',color=colors_dark[1],y=0.92,x=0.28,alpha=0.8)
+# fig,ax=plt.subplots(1,1,figsize=(14,7))
+# sns.heatmap(confusion_matrix(y_test_new_incep,pred_ready_incep),ax=ax,xticklabels=labels,yticklabels=labels,annot=True, fmt='g',
+#            cmap=colors_green[::-1],alpha=0.7,linewidths=2,linecolor=colors_dark[3])
+# fig.text(s='Heatmap of the Confusion Matrix',size=18,fontweight='bold',
+#              fontname='monospace',color=colors_dark[1],y=0.92,x=0.28,alpha=0.8)
 
-# # plt.show()
+# plt.show()
 
-# test_loss, test_acc = my_model_inception.evaluate(X_test, y_test, verbose=2)
+test_loss, test_acc = my_model_inception.evaluate(X_test, y_test, verbose=2)
 
-# print(f' Test accuracy: {test_acc:.3f} \n Test loss {test_loss:.3f}')
+print(f' Test accuracy: {test_acc:.3f} \n Test loss {test_loss:.3f}')
 
-# my_model_densenet = load_model('/home/viktoriia.trokhova/master_4/<keras.engine.functional.Functional object at 0x7f06ec417370>.h5')
-# print('densenet')
-# pred_dense = my_model_densenet.predict(X_test)
-# pred_ready_dense = np.argmax(pred_dense,axis=1)
-# y_test_new_dense = np.argmax(y_test,axis=1)
+my_model_densenet = load_model('/home/viktoriia.trokhova/master_4/<keras.engine.functional.Functional object at 0x7f06ec417370>.h5')
+print('densenet')
+pred_dense = my_model_densenet.predict(X_test)
+pred_ready_dense = np.argmax(pred_dense,axis=1)
+y_test_new_dense = np.argmax(y_test,axis=1)
 
 
-# # print(classification_report(y_test_new_dense,pred_ready_dense))
+# print(classification_report(y_test_new_dense,pred_ready_dense))
 
-# # fig,ax=plt.subplots(1,1,figsize=(14,7))
-# # sns.heatmap(confusion_matrix(y_test_new_dense,pred_ready_dense),ax=ax,xticklabels=labels,yticklabels=labels,annot=True, fmt='g',
-# #            cmap=colors_green[::-1],alpha=0.7,linewidths=2,linecolor=colors_dark[3])
-# # fig.text(s='Heatmap of the Confusion Matrix',size=18,fontweight='bold',
-# #              fontname='monospace',color=colors_dark[1],y=0.92,x=0.28,alpha=0.8)
+# fig,ax=plt.subplots(1,1,figsize=(14,7))
+# sns.heatmap(confusion_matrix(y_test_new_dense,pred_ready_dense),ax=ax,xticklabels=labels,yticklabels=labels,annot=True, fmt='g',
+#            cmap=colors_green[::-1],alpha=0.7,linewidths=2,linecolor=colors_dark[3])
+# fig.text(s='Heatmap of the Confusion Matrix',size=18,fontweight='bold',
+#              fontname='monospace',color=colors_dark[1],y=0.92,x=0.28,alpha=0.8)
 
-# # plt.show()
+# plt.show()
 
-# test_loss, test_acc = my_model_densenet.evaluate(X_test, y_test, verbose=2)
+test_loss, test_acc = my_model_densenet.evaluate(X_test, y_test, verbose=2)
 
-# print(f' Test accuracy: {test_acc:.3f} \n Test loss {test_loss:.3f}')
+print(f' Test accuracy: {test_acc:.3f} \n Test loss {test_loss:.3f}')
 
 # # inception: <keras.engine.functional.Functional object at 0x7f07681711b0>.h5
 # # densenet: <keras.engine.functional.Functional object at 0x7f06ec417370>.h5
