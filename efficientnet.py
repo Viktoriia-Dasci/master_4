72cedfbf29e4d36a41faf0df827c95ebcfc2d25c
Viktoriia-Dasci <74453524+Viktoriia-Dasci@users.noreply.github.com>
2023-03-17 22:31:27 +0100

    Update efficientnet.py

--- efficientnet.py
+++ efficientnet.py
@@ -660,7 +660,7 @@ best_model_wts = {}
 #      return max_accuracy
   
   
-EPOCHS = 30
+EPOCHS = 50
     
 # study = optuna.create_study(direction="maximize", sampler=optuna.samplers.TPESampler(), pruner=optuna.pruners.HyperbandPruner(min_resource=1, max_resource=6, reduction_factor=5))
 # study.optimize(objective, n_trials=30)
