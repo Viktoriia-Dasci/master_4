d2f1d67945163e839f892908aed79f1a7aa44e3b
Viktoriia-Dasci <74453524+Viktoriia-Dasci@users.noreply.github.com>
2023-03-19 15:50:12 +0100

    Update resnett2.py

--- resnett2.py
+++ resnett2.py
@@ -163,7 +163,7 @@ class MyCustomResnet50(nn.Module):
 # Define the transformation to be applied to the images
 transform = transforms.Compose([transforms.ToTensor(),
                                 transforms.Resize((224,224)),                                  
-                                torchvision.transforms.Normalize(
+                                transforms.Normalize(
                                    mean=[0.485, 0.456, 0.406],
                                    std=[0.229, 0.224, 0.225],),
 ])
