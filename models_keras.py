033b36e6de4ac51fe8c89cbc963ff3208d95d59a
Viktoriia-Dasci <74453524+Viktoriia-Dasci@users.noreply.github.com>
2023-04-06 19:58:06 +0200

    Update models_keras.py

--- models_keras.py
+++ models_keras.py
@@ -303,16 +303,16 @@ tuner = Hyperband(
     hyperband_iterations=10
 )
 
-# tuner.search(train_generator,
-#              validation_data=(X_val, y_val),
-#              steps_per_epoch=len(train_generator),
-#              epochs=50,
-#              verbose=1
-#              )
+tuner.search(train_generator,
+             validation_data=(X_val, y_val),
+             steps_per_epoch=len(train_generator),
+             epochs=50,
+             verbose=1
+             )
 
-# Print the best hyperparameters found by the tuner
-#best_hyperparams = tuner.get_best_hyperparameters(1)[0]
-#print(f'Best hyperparameters: {best_hyperparams}')
+Print the best hyperparameters found by the tuner
+best_hyperparams = tuner.get_best_hyperparameters(1)[0]
+print(f'Best hyperparameters: {best_hyperparams}')
 
 # Get the best model found by the tuner
 best_model = tuner.get_best_models(1)[0]
