5687125847488577bb726c45c740041c6ff77a38
Viktoriia-Dasci <74453524+Viktoriia-Dasci@users.noreply.github.com>
2023-04-09 01:32:22 +0200

    Update models_keras.py

--- models_keras.py
+++ models_keras.py
@@ -262,7 +262,7 @@ from kerastuner.engine.hyperparameters import HyperParameters
 #     return model
 
 def model_resnet():
-    model_name = tf.keras.applications.resnet50.ResNet50(include_top=False, weights='imagenet', input_shape=(224,224,3), classes=2))
+    model_name = tf.keras.applications.resnet50.ResNet50(include_top=False, weights='imagenet', input_shape=(224,224,3), classes=2)
     model = model_name.output
     model = tf.keras.layers.GlobalAveragePooling2D()(model)
     model = tf.keras.layers.Dense(128, activation='relu')(model)
