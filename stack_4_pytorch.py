ef19d43203abdc1f1f87e0ea1fcfa2134cc33041
Viktoriia-Dasci <74453524+Viktoriia-Dasci@users.noreply.github.com>
2023-06-10 22:19:08 +0200

    Update stack_4_pytorch.py

--- stack_4_pytorch.py
+++ stack_4_pytorch.py
@@ -233,7 +233,7 @@ from torchvision import transforms
 # Define the transformation to be applied to the images
 transform = transforms.Compose([
     transforms.ToTensor(),
-    transforms.CenterCrop((224, 224)),
+    transforms.Resize((224, 224)),
     transforms.Normalize(
         mean=[0.485, 0.456, 0.406],
         std=[0.229, 0.224, 0.225]
