1b72bc8ac80a19639164565c4138e6ea98708a9f
Viktoriia-Dasci <74453524+Viktoriia-Dasci@users.noreply.github.com>
2023-06-15 23:07:30 +0200

    Update old_grad.py

--- old_grad.py
+++ old_grad.py
@@ -517,7 +517,7 @@ def train_and_evaluate(param, model, trial):
             train_input = train_input.to(device)
             train_mask = train_mask.to(device)
             targets = torch.argmax(train_label, dim=1)
-            output, targets_, xe_loss_, gcam_losses_ = model(train_input, targets, train_mask, batch_size=train_input.size(0), dropout=nn.Dropout(param['drop_out']))
+            output, targets_, xe_loss_, gcam_losses_ = model(train_input, targets, train_mask, batch_size=train_input.size(0), dropout=nn.Dropout(param['dropout']))
            
             
             batch_loss = xe_loss_.mean() + param['lambda_val'] * gcam_losses_
@@ -567,7 +567,7 @@ def train_and_evaluate(param, model, trial):
             val_input = val_input.to(device)
             val_mask = val_mask.to(device)
             val_targets = torch.argmax(val_label, dim=1)
-            output, targets_, xe_loss_, gcam_losses_ = model(val_input, val_targets, val_mask, batch_size=val_input.size(0), dropout=nn.Dropout(param['drop_out']))
+            output, targets_, xe_loss_, gcam_losses_ = model(val_input, val_targets, val_mask, batch_size=val_input.size(0), dropout=nn.Dropout(param['dropout']))
             
             batch_loss = xe_loss_.mean() + param['lambda_val'] * gcam_losses_
             total_loss_val += batch_loss.item()
