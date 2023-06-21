f1231e47849e5be4481a6ce05105f15d7bc68646
Viktoriia-Dasci <74453524+Viktoriia-Dasci@users.noreply.github.com>
2023-06-21 22:25:10 +0200

    Update test.py

--- test.py
+++ test.py
@@ -10,42 +10,31 @@ from torch.utils import data
 from torch.utils.data import Dataset, DataLoader
 from torch.autograd import Variable
 from torch import tensor
-
 import torchvision.models as models
 import torchvision
 from torchvision import datasets, models, transforms
 from torchvision.models import ResNet50_Weights
-
 import glob
 import os
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-
 import matplotlib.pyplot as plt
 #from PIL import Image
 import cv2
 import seaborn as sns
-
-
 from sklearn.utils import shuffle
 from sklearn import preprocessing as p
 from sklearn.metrics import RocCurveDisplay
 from sklearn.metrics import confusion_matrix, roc_curve, auc
-
-
 from torchmetrics.classification import ROC
 from torchmetrics import AUROC
-
 import optuna
 from optuna.trial import TrialState
-
 from segmentation_models_pytorch import losses
 dice_loss = losses.DiceLoss('binary')
 foc_loss = losses.FocalLoss('binary')
-
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
 val_transforms = transforms.Compose([torchvision.transforms.ToTensor(),
                                       transforms.Resize((224,224)),
                                       torchvision.transforms.Normalize(
@@ -53,10 +42,8 @@ val_transforms = transforms.Compose([torchvision.transforms.ToTensor(),
                                           std=[0.229, 0.224, 0.225],
     ),
                                       ])
-
 class myDataset_test(Dataset):
-
-    def __init__(self, transform=None):
+    def __init__(self, transform=None): 
         #folder containing class folders with images
         self.imgs_path = "/home/viktoriia.trokhova/T2_new_MRI_slices/test/"
         self.masks_path = "/home/viktoriia.trokhova/T2_new_Msk_slices/test/"
@@ -68,39 +55,33 @@ class myDataset_test(Dataset):
         self.masks = []
         for class_path in file_list:
             class_name = class_path.split("/")[-1]
-            for img_path in sorted(glob.glob(class_path + "/*.npy")):
+            for img_path in sorted(glob.glob(class_path + "/*")):
                 self.images.append(img_path)
-            for img_path in sorted(glob.glob(class_path + "/*.npy")):
+            for img_path in sorted(glob.glob(class_path + "/*")):
                 self.targets.append(class_name)
         for msk_path in msk_list:
-            for masks_path in sorted(glob.glob(msk_path + "/*.npy")):
+            for masks_path in sorted(glob.glob(msk_path + "/*")):
                   self.masks.append(masks_path)
         self.images, self.targets, self.masks = shuffle(self.images, self.targets, self.masks, random_state=101)
         print(self.images[-100])
         print(self.targets[-100])
         print(self.masks[-100])
-        # print(len(self.images))
-        # print(len(self.targets))
-        # print(len(self.masks))
+        print(len(self.images))
+        print(len(self.targets))
+        print(len(self.masks))
         self.class_map = {"HGG_t2" : 0, "LGG_t2": 1}
         self.img_dim = (224, 224)
-
     def __len__(self):
         return len(self.images)
-
     def __getitem__(self, idx):
         img_path  = self.images[idx]
         class_name = self.targets[idx]
         masks_path = self.masks[idx]
         img = np.load(img_path)
         msk = np.load(masks_path)
-        reshap_img = img.reshape(-1, 3)
         min_max_scaler = p.MinMaxScaler()
-        img_t = min_max_scaler.fit_transform(reshap_img)
-        img = img_t.reshape(img.shape)
-        reshap_msk = msk.reshape(-1, 3)
-        msk_t = min_max_scaler.fit_transform(reshap_msk)
-        msk = msk_t.reshape(msk.shape)
+        img = min_max_scaler.fit_transform(img)
+        msk = min_max_scaler.fit_transform(msk)
         img_float32 = np.float32(img)
         img_color = cv2.cvtColor(img_float32, cv2.COLOR_GRAY2RGB)
         img_tensor = val_transforms(img_color)
@@ -109,13 +90,9 @@ class myDataset_test(Dataset):
         msk_tensor = val_transforms(msk_color)
         class_id = self.class_map[class_name]
         class_id = torch.tensor(class_id)
-        class_id_one_hot = F.one_hot(class_id, num_classes=2).float()
-
-        return img_tensor, class_id_one_hot, msk_tensor
-
-
+    
+        return img_tensor, class_id, msk_tensor
 from torchvision.models import densenet121
-
 class MyCustomDenseNet121(nn.Module):
     def __init__(self, pretrained=True, dense_0_units=None, dense_1_units=None):
         super().__init__()
@@ -124,7 +101,6 @@ class MyCustomDenseNet121(nn.Module):
         self.features = densenet.features
         in_features = densenet.classifier.in_features
         self.last_pooling_operation = nn.AdaptiveAvgPool2d((1, 1))
-
         if dense_0_units is not None:
             dense_0_units = int(dense_0_units)
             self.fc1 = nn.Linear(in_features, dense_0_units, bias=True)
@@ -137,7 +113,6 @@ class MyCustomDenseNet121(nn.Module):
             self.fc2 = None
             self.fc_final = nn.Linear(dense_0_units, 2)
 
-            
     def forward(self, input_imgs, targets=None, masks=None, batch_size=None, xe_criterion=nn.CrossEntropyLoss(weight=tensor([0.6293, 2.4333], device='cuda:0')), dropout=None):
         images_feats = self.features(input_imgs)
         output = self.last_pooling_operation(images_feats)
@@ -151,7 +126,6 @@ class MyCustomDenseNet121(nn.Module):
         
         images_outputs = self.fc_final(output)
         
-
         
         # # compute gcam for images
         orig_gradcam_mask = compute_gradcam(images_outputs, images_feats, targets)
@@ -186,8 +160,6 @@ class MyCustomDenseNet121(nn.Module):
         xe_loss = xe_criterion(images_outputs, targets)
         
         return images_outputs, targets, xe_loss, gcam_losses      #return images_outputs
-
-
 def compute_gradcam(output, feats, target):
     """
     Compute normalized Grad-CAM for the given target using the model output and features
@@ -214,51 +186,29 @@ def compute_gradcam(output, feats, target):
     spatial_sum1 = gradcam.sum(dim=[1, 2]).unsqueeze(-1).unsqueeze(-1)
     gradcam = (gradcam / (spatial_sum1 + eps)) + eps
     return gradcam
-
-
 model = MyCustomDenseNet121(pretrained=True, dense_0_units=128).to(device)  
-
-
 model.load_state_dict(torch.load('/home/viktoriia.trokhova/model_weights/model_best.pt'), strict=False)
-
 test_dataset = myDataset_test(transform = None)
 test_dataloader = torch.utils.data.DataLoader(myDataset_test(transform = None),
-                                    batch_size=16,
+                                    batch_size=64,
                                     shuffle=False,
                                     num_workers=0)
-
-from sklearn.metrics import f1_score
 model.eval()
 running_loss = 0.0
-test_f1 = 0.0
 running_corrects = 0.0
 for inputs, labels, masks in test_dataloader:
     inputs = inputs.to(device)
     labels = labels.to(device)
-    test_labels = torch.argmax(labels, dim=1).to(device)
     masks = masks.to(device)
-
-    outputs, targets_, xe_loss_, gcam_losses_  = model(inputs, test_labels, masks, batch_size = inputs.size(0), dropout=nn.Dropout(0.8))
-
-    loss = xe_loss_.mean() + 0.663 * gcam_losses_
-    running_loss += loss
-
-    outputs = F.softmax(outputs, dim=1)
-    
-    predictions = torch.argmax(outputs, dim=1).detach().cpu().numpy()
-    target_numpy = labels.detach().cpu().numpy()
-    correct_predictions = np.sum(predictions == target_numpy.argmax(axis=1))
-    
-    batch_accuracy = correct_predictions / target_numpy.shape[0]
-    running_corrects += batch_accuracy
-    
-    f1 = f1_score(target_numpy.argmax(axis=1), predictions, average='macro')
-    test_f1 += f1
-    print(f1)
-
-    
-epoch_val_loss = running_loss / len(test_dataloader)
-epoch_val_accuracy = correct_predictions / len(test_dataloader)
-epoch_val_f1_score = test_f1 / len(test_dataloader)
-print('val f1-score:', epoch_val_f1_score)
-print('val accuracy:', epoch_val_accuracy)
+  
+    outputs, targets_, xe_loss_, gcam_losses_ = model(inputs, labels, masks, batch_size = inputs.size(0), dropout=nn.Dropout(0.8))
+    loss = xe_loss_.mean() + 0.663 * gcam_losses_.mean()
+    #loss = xe_loss_.mean()
+   
+    _, preds = torch.max(outputs, 1)  
+    running_loss += loss.item() * inputs.size(0)
+    running_corrects += torch.sum(preds == labels.data)
+epoch_loss = running_loss / 1716
+epoch_acc = running_corrects.double() / 1716
+print('Test loss: {:.4f}, acc: {:.4f}'.format(epoch_loss,
+                                            epoch_acc))
