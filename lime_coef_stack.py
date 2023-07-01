271813ddb8585012be908e88ec74f3e52b5cc8a4
Viktoriia-Dasci <74453524+Viktoriia-Dasci@users.noreply.github.com>
2023-07-02 00:29:17 +0200

    Update lime_coef_stack.py

--- lime_coef_stack.py
+++ lime_coef_stack.py
@@ -356,9 +356,9 @@ lime_list = []
 #df = pd.DataFrame(columns=["img_class", "img_num", "pat_num", "dice_coef"])
 
 def lime_coef(img_class, img_msk_class):
-    img_dir = "/content/drive/MyDrive/Stacked_MRI_new/test/" + img_class + "_stack"
+    img_dir = "/home/viktoriia.trokhova/Stacked_MRI_new/test/" + img_class + "_stack"
     img_files = os.listdir(img_dir)
-    msk_dir = "/content/drive/MyDrive/Stacked_Msk_new/test/" + img_class + "_masks"
+    msk_dir = "/home/viktoriia.trokhova/Stacked_MRI_new/test/" + img_class + "_masks"
     msk_files = os.listdir(msk_dir)
 
     # Get the last number of the file names to set pat_num
