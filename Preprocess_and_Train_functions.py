24632bd2d28f746ae92414b2b5f23f147b483068
Viktoriia-Dasci <74453524+Viktoriia-Dasci@users.noreply.github.com>
2023-07-11 23:57:55 +0200

    Create Functions_keras.py

---
+++ Preprocess_and_Train_functions.py
@@ -0,0 +1,159 @@
+def save_to_dir(slices, path):
+      for i in range(len(slices)):
+          img = slices[i]
+          save_path = path + '/' + str(i)
+          np.save(save_path, img)
+      
+def load_from_dir(path):
+      file_paths = glob.glob(os.path.join(path, '*.npy'))
+   
+      slices_list=[]
+      for img in range(len(file_paths)):
+          new_img = np.load(file_paths[img])
+          slices_list.append(new_img)
+      return slices_list
+    
+    
+def add_labels(X, y, images_list, label):
+  
+    for img in images_list:
+      X.append(img)
+      y.append(label)
+    return X, y
+  
+  
+def plot_acc_loss_f1_auc(model_history, folder_path, modality):
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+        
+    loss = model_history.history['loss']
+    val_loss = model_history.history['val_loss']
+    epochs = range(1, len(loss) + 1)
+    plt.plot(epochs, loss, 'y', label='Training loss')
+    plt.plot(epochs, val_loss, 'r', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.savefig(os.path.join(folder_path, f'loss_{modality}.png'))
+    plt.close()
+    
+    acc = model_history.history['accuracy']
+    val_acc = model_history.history['val_accuracy']
+    plt.plot(epochs, acc, 'y', label='Training accuracy')
+    plt.plot(epochs, val_acc, 'r', label='Validation accuracy')
+    plt.title('Training and validation accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.savefig(os.path.join(folder_path, f'accuracy_{modality}.png'))
+    plt.close()
+    f1 = model_history.history['f1_score']
+    val_f1 = model_history.history['val_f1_score']
+    plt.plot(epochs, f1, 'y', label='Training F1 Score')
+    plt.plot(epochs, val_f1, 'r', label='Validation F1 Score')
+    plt.title('Training and validation F1 Score')
+    plt.xlabel('Epochs')
+    plt.ylabel('F1 Score')
+    plt.legend()
+    plt.savefig(os.path.join(folder_path, f'f1_score_{modality}.png'))
+    plt.close()
+
+
+def preprocess(images_list):
+    list_new = []
+    for img in images_list:
+        img_color = cv2.cvtColor(img.astype(np.float32), cv2.COLOR_GRAY2RGB)
+        #img_res = cv2.resize(img, (224, 224))
+        img_cropped = tf.image.crop_to_bounding_box(img_color, 8, 8, 224, 224)
+        img_processed = tf.keras.applications.imagenet_utils.preprocess_input(img_cropped)
+        list_new.append(img_processed)
+    return list_new
+
+#Adapted from 'https://gist.github.com/angeligareta/83d9024c5e72ac9ebc34c9f0b073c64c'
+def generate_class_weights(class_series, multi_class=True, one_hot_encoded=False):  
+  if multi_class:
+    # If class is one hot encoded, transform to categorical labels to use compute_class_weight   
+    if one_hot_encoded:
+      class_series = np.argmax(class_series, axis=1)
+  
+    # Compute class weights with sklearn method
+    class_labels = np.unique(class_series)
+    class_weights = compute_class_weight(class_weight='balanced', classes=class_labels, y=class_series)
+    return dict(zip(class_labels, class_weights))
+  else:
+    # It is neccessary that the multi-label values are one-hot encoded
+    mlb = None
+    if not one_hot_encoded:
+      mlb = MultiLabelBinarizer()
+      class_series = mlb.fit_transform(class_series)
+    n_samples = len(class_series)
+    n_classes = len(class_series[0])
+    # Count each class frequency
+    class_count = [0] * n_classes
+    for classes in class_series:
+        for index in range(n_classes):
+            if classes[index] != 0:
+                class_count[index] += 1
+    
+    # Compute class weights using balanced method
+    class_weights = [n_samples / (n_classes * freq) if freq > 0 else 1 for freq in class_count]
+    class_labels = range(len(class_weights)) if mlb is None else mlb.classes_
+    return dict(zip(class_labels, class_weights))
+
+
+def focal_loss(y_true, y_pred, gamma=2.0):
+    epsilon = tf.keras.backend.epsilon()
+    y_pred = tf.clip_by_value(y_pred, epsilon, 1.0 - epsilon)
+    
+    # Calculate focal loss
+    cross_entropy = -y_true * tf.math.log(y_pred)
+    focal_loss = tf.pow(1.0 - y_pred, gamma) * cross_entropy
+    
+    return tf.reduce_mean(focal_loss, axis=-1)      
+
+
+def f1_score(y_true, y_pred):
+    y_pred = tf.argmax(y_pred, axis=-1)
+    y_true = tf.argmax(y_true, axis=-1)
+    tp = tf.reduce_sum(tf.cast(tf.logical_and(tf.equal(y_true, 1), tf.equal(y_pred, 1)), dtype=tf.float32))
+    fp = tf.reduce_sum(tf.cast(tf.logical_and(tf.equal(y_true, 0), tf.equal(y_pred, 1)), dtype=tf.float32))
+    fn = tf.reduce_sum(tf.cast(tf.logical_and(tf.equal(y_true, 1), tf.equal(y_pred, 0)), dtype=tf.float32))
+    precision = tp / (tp + fp + tf.keras.backend.epsilon())
+    recall = tp / (tp + fn + tf.keras.backend.epsilon())
+    f1 = 2 * precision * recall / (precision + recall + tf.keras.backend.epsilon())
+    return f1
+
+
+def model_train(model_name, save_name, image_size, dropout, optimizer, dense_0_units, dense_1_units, batch_size):
+    model = model_name.output
+    model = tf.keras.layers.GlobalAveragePooling2D()(model)
+    model = tf.keras.layers.Dropout(rate=dropout)(model)
+    model = tf.keras.layers.Dense(dense_0_units, activation='relu')(model)
+    model = tf.keras.layers.Dense(dense_1_units, activation='relu')(model)
+    model = tf.keras.layers.Dense(2, activation='softmax')(model)
+    model = tf.keras.models.Model(inputs=model_name.input, outputs=model)
+    model.compile(loss=focal_loss, optimizer=optimizer, metrics=['accuracy', f1_score])
+    
+    checkpoint = ModelCheckpoint("/home/viktoriia.trokhova/model_weights/" + save_name + ".h5", monitor='val_f1_score', save_best_only=True, mode="max", verbose=1)
+    early_stop = EarlyStopping(monitor='val_f1_score', mode='max', patience=10, verbose=1, restore_best_weights=True)
+    reduce_lr = ReduceLROnPlateau(monitor='val_f1_score', factor=0.3, patience=5, min_delta=0.001, mode='max', verbose=1)
+    history = model.fit(train_generator, validation_data=(X_val, y_val), epochs=50, batch_size=batch_size, verbose=1, callbacks=[checkpoint, early_stop, reduce_lr], class_weight=class_weights)
+
+    
+        
+    train_loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    train_accuracy = history.history['accuracy']
+    val_accuracy = history.history['val_accuracy']
+    train_f1_score = history.history['f1_score']
+    val_f1_score = history.history['val_f1_score']
+
+    print("Train Loss:", train_loss)
+    print("Val Loss:", val_loss)
+    print("Train Accuracy:", train_accuracy)
+    print("Val Accuracy:", val_accuracy)
+    print("Train F1 Score:", train_f1_score)
+    print("Val F1 Score:", val_f1_score)  
+      
+    return history
