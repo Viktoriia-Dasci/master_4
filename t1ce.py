5a7fecfe03aec7b559f7d20c163cbb8f2f2353fc
Viktoriia-Dasci <74453524+Viktoriia-Dasci@users.noreply.github.com>
2023-07-13 16:08:10 +0200

    Update t1ce.py

--- t1ce.py
+++ t1ce.py
@@ -1,35 +1,43 @@
 import numpy as np
+import os
+import random
 import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.applications import EfficientNetB0
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+from tensorflow.keras.models import Model
+from tensorflow.keras import backend as K
+from tensorflow.keras import layers
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+import tensorflow_addons as tfa
 from sklearn.utils import shuffle
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow_addons import metrics
+import keras_tuner
 from kerastuner.tuners import Hyperband
 from kerastuner.engine.hyperparameters import HyperParameters
-from tensorflow.keras.applications import InceptionV3, DenseNet121
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.applications import EfficientNetB0
-import os
-import glob
-import numpy as np
-import matplotlib.pyplot as plt
-import cv2
-import tensorflow as tf
-from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
-from sklearn.utils.class_weight import compute_class_weight
-from sklearn.preprocessing import MultiLabelBinarizer
 #custom functions
-from Functions import load_from_dir, preprocess, add_labels, generate_class_weights, model_train, plot_acc_loss_f1
+from Model_functions import *
+
+home_dir = '/home/viktoriia.trokhova/'
+
+base_dir = '/home/viktoriia.trokhova/Split_data/'
+
+modality = 't1ce'
 
 #load data
-HGG_list_train = load_from_dir('/content/drive/MyDrive/Split_data/t1ce_mri_slices/train/HGG_t1ce')
-LGG_list_train = load_from_dir('/content/drive/MyDrive/Split_data/t1ce_mri_slices/train/LGG_t1ce')
-HGG_list_val = load_from_dir('/content/drive/MyDrive/Split_data/t1ce_mri_slices/val/HGG_t1ce')
-LGG_list_val = load_from_dir('/content/drive/MyDrive/Split_data/t1ce_mri_slices/val/LGG_t1ce')
-HGG_list_test = load_from_dir('/content/drive/MyDrive/Split_data/t1ce_mri_slices/test/HGG_t1ce')
-LGG_list_test = load_from_dir('/content/drive/MyDrive/Split_data/t1ce_mri_slices/test/LGG_t1ce')
+HGG_list_train = load_from_dir(f'/content/drive/MyDrive/Split_data/{modality}_mri_slices/train/HGG_{modality}')
+LGG_list_train = load_from_dir(f'/content/drive/MyDrive/Split_data/{modality}_mri_slices/train/LGG_{modality}')
+HGG_list_val = load_from_dir(f'/content/drive/MyDrive/Split_data/{modality}_mri_slices/val/HGG_{modality}')
+LGG_list_val = load_from_dir(f'/content/drive/MyDrive/Split_data/{modality}_mri_slices/val/LGG_{modality}')
+HGG_list_test = load_from_dir(f'/content/drive/MyDrive/Split_data/{modality}_mri_slices/test/HGG_{modality}')
+LGG_list_test = load_from_dir(f'/content/drive/MyDrive/Split_data/{modality}_mri_slices/test/LGG_{modality}')
 
 
-#preprocess data
+#preprocessing data
 HGG_list_new_train = preprocess(HGG_list_train)
 LGG_list_new_train = preprocess(LGG_list_train)
 
@@ -39,7 +47,7 @@ LGG_list_new_val = preprocess(LGG_list_val)
 HGG_list_new_test = preprocess(HGG_list_test)
 LGG_list_new_test = preprocess(LGG_list_test)
 
-# Combine the HGG and LGG lists
+# Combining the HGG and LGG lists
 X_train, y_train = add_labels([], [], HGG_list_new_train, label='HGG')
 X_train, y_train = add_labels(X_train, y_train, LGG_list_new_train, label='LGG')
 
@@ -49,17 +57,18 @@ X_val, y_val = add_labels(X_val, y_val, LGG_list_new_val, label='LGG')
 X_test, y_test = add_labels([], [], HGG_list_new_test, label='HGG')
 X_test, y_test = add_labels(X_test, y_test, LGG_list_new_test, label='LGG')
 
-# Convert labels to numerical values and one-hot encoding
+# Converting labels to numerical values and one-hot encoding
 labels = {'HGG': 0, 'LGG': 1}
 y_train = tf.keras.utils.to_categorical([labels[y] for y in y_train])
 y_val = tf.keras.utils.to_categorical([labels[y] for y in y_val])
 y_test = tf.keras.utils.to_categorical([labels[y] for y in y_test])
 
-# Convert data to arrays and shuffle
+# Converting data to arrays and shuffle
 X_val, y_val = shuffle(np.array(X_val), y_val, random_state=101)
 X_train, y_train = shuffle(np.array(X_train), y_train, random_state=101)
 X_test, y_test = shuffle(np.array(X_test), y_test, random_state=101)
 
+#Calculating class_weights
 class_weights = generate_class_weights(y_train, multi_class=False, one_hot_encoded=True)
 print(class_weights)
 
@@ -68,6 +77,8 @@ print(y_train.shape)
 print(X_val.shape)
 print(y_val.shape)
 
+
+#Data augmentation
 datagen = ImageDataGenerator(
     rotation_range=90,
     vertical_flip=True,
@@ -78,6 +89,7 @@ train_generator = datagen.flow(
     X_train, y_train,
     shuffle=True)
 
+#Hyperparameter tuning
 hp = HyperParameters()
 
 tuner_effnet = Hyperband(
@@ -107,39 +119,49 @@ tuner_inception = Hyperband(
     hyperband_iterations=5
 )
 
-# Search hyperparameters
-tuners = [tuner_effnet, tuner_densenet, tuner_inception]
-for tuner in tuners:
+# Searching for best hyperparameters and models for each tuner
+best_hyperparameters = {}
+best_models = {}
+tuners = {'EffNet': tuner_effnet, 'DenseNet': tuner_densenet, 'Inception': tuner_inception}
+
+for name, tuner in tuners.items():
     tuner.search(train_generator,
                  validation_data=(X_val, y_val),
                  steps_per_epoch=len(train_generator),
                  epochs=50,
                  verbose=1
                  )
-    # Print the best hyperparameters found by the tuner
-    best_hyperparams = tuner.get_best_hyperparameters(1)[0]
-    print(f'Best hyperparameters: {best_hyperparams}')
+    
+    best_hyperparameters[name] = tuner.get_best_hyperparameters(1)[0]
+    best_models[name] = tuner.get_best_models(1)[0]
 
-#Get the best model found by the tuner
-best_model = tuner.get_best_models(1)[0]
-
-checkpoint = ModelCheckpoint("/home/viktoriia.trokhova/model_weights/model_tuned" + ".h5",monitor='val_f1_score',save_best_only=True,mode="max",verbose=1)
+# Define callbacks
+checkpoint = ModelCheckpoint(f"{home_dir}/model_weights/model_tuned.h5",monitor='val_f1_score',save_best_only=True,mode="max",verbose=1)
 early_stop = EarlyStopping(monitor='val_f1_score', mode='max', patience=10, verbose=1, restore_best_weights=True)
 reduce_lr = ReduceLROnPlateau(monitor = 'val_f1_score', factor = 0.3, patience = 2, min_delta = 0.001, mode='max',verbose=1)
 
-#Fit the model to the training data for 50 epochs using the best hyperparameters
-history_effnet = best_model.fit(
-    train_generator,
-    epochs=50,
-    validation_data=(X_val, y_val),
-    verbose=1,
-    callbacks=[checkpoint, early_stop, reduce_lr]
-)
+# Define the path for saving the plots
+plot_folder_path = os.path.join(home_dir, f"/model_plots/{modality}") 
+
+# Fit the best model from each tuner to the training data for 50 epochs using the best hyperparameters
+for name, model in best_models.items():
+    print(f'Training {name}...')
+    history = model.fit(
+        train_generator,
+        epochs=50,
+        validation_data=(X_val, y_val),
+        verbose=1,
+        callbacks=[checkpoint, early_stop, reduce_lr]
+    )
+
+    # After training, plot the accuracy, loss, and f1 score
+    plot_acc_loss_f1(history, plot_folder_path, name)
 
 
-history_inception_weights = model_train(model_name = tf.keras.applications.inception_v3.InceptionV3(include_top=False, weights='imagenet', input_shape=(224,224,3), classes=2), save_name = "inception_t1ce", image_size = 224, dropout=0.7, optimizer=tf.keras.optimizers.SGD(learning_rate=0.1), dense_0_units=16, dense_1_units=80, batch_size=64)  
-history_effnet = model_train(model_name = EfficientNetB0(weights='imagenet', include_top=False, input_shape=(224,224,3)), save_name = "effnet_t1ce", image_size = 224, dropout=0.6, optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), dense_0_units=48, dense_1_units=80, batch_size=64)  
-history_densenet_weights = model_train(model_name = tf.keras.applications.densenet.DenseNet121(include_top=False, weights='imagenet', input_shape=(224,224,3), classes=2), save_name = "densenet_t1ce", image_size = 224, dropout=0.2, optimizer=tf.keras.optimizers.SGD(learning_rate=0.01), dense_0_units=96, dense_1_units=None, batch_size=32)  
-plot_acc_loss_f1_auc(history_inception_weights,  '/home/viktoriia.trokhova/plots/inception')
-plot_acc_loss_f1_auc(history_densenet_weights,  '/home/viktoriia.trokhova/plots/densenet')
-plot_acc_loss_f1_auc(history_effnet,  '/home/viktoriia.trokhova/plots/effnet')
+history_inception_weights = model_train(model_name = tf.keras.applications.inception_v3.InceptionV3(include_top=False, weights='imagenet', input_shape=(224,224,3), classes=2), save_name = f"inception_{modality}", image_size = 224, dropout=0.7, optimizer=tf.keras.optimizers.SGD(learning_rate=0.1), dense_0_units=16, dense_1_units=80, batch_size=64)  
+history_effnet = model_train(model_name = EfficientNetB0(weights='imagenet', include_top=False, input_shape=(224,224,3)), save_name = f"effnet_{modality}", image_size = 224, dropout=0.6, optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), dense_0_units=48, dense_1_units=80, batch_size=64)  
+history_densenet_weights = model_train(model_name = tf.keras.applications.densenet.DenseNet121(include_top=False, weights='imagenet', input_shape=(224,224,3), classes=2), save_name = f"densenet_{modality}", image_size = 224, dropout=0.2, optimizer=tf.keras.optimizers.SGD(learning_rate=0.01), dense_0_units=96, dense_1_units=None, batch_size=32)  
+
+plot_acc_loss_f1_auc(history_inception_weights,  f'{home_dir}/plots/inception')
+plot_acc_loss_f1_auc(history_densenet_weights,  f'{home_dir}/plots/densenet')
+plot_acc_loss_f1_auc(history_effnet,  f'{home_dir}/plots/effnet')
